dcce8f8058950f653412ce2b35e1bcbd404b007d
Victoria Jackson <vjack2@email.unc.edu>
2023-09-20 18:23:50 -0400

    Progress on exercise 2

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,45 @@
+"""EX02- One-Shot Wordle-Loops!"""
+__author__ = "730400711"
+
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
+secret: str = "python"
+guess: str = str(input(f"What is your {len(secret)}-letter guess? "))
+
+
+while len(guess) != len(secret):
+# Reassigning guess variable to ask for player input again
+    guess = input(f"That was not {len(secret)} letters! Try again: ")
+# Checking to see if any guessed characters are present in the indices of the secret
+index: int = 0
+emoji: str = ""
+
+while index < len(secret):
+ # Checking to see if index of guess is equal to that of the secret
+    if guess[index] == secret[index]:
+        emoji = emoji + GREEN_BOX
+# Setting up boolean for characters in guess. Creating count for guessed characters in wrong place.
+    else:
+        guessed_chr: bool = False
+        alt_index: int = 0
+        while not guessed_chr and alt_index < len(secret):
+            if secret[alt_index] == guess[index]:
+                guessed_chr = True
+            else:
+                alt_index = alt_index + 1
+# Checking guessed characters that are true but in wrong spot and those that are not true at all.
+        if guessed_chr:
+            emoji = emoji + YELLOW_BOX
+        else:
+            emoji = emoji + WHITE_BOX
+    index = index +1
+
+print(emoji)
+
+if guess == secret:
+    print("Woo! You got it! ")
+else:
+    print("Not quite. Play again soon! ")
+    
---
+++ lessons/guessing_game.py
@@ -0,0 +1,27 @@
+""" Program that loops until correct number is guessed"""
+
+secret: int = 5
+guess: int = int(input("Guess a number between 1 and 10: "))
+number_of_tries: int = 0
+max_tries: int = 3
+
+while (guess != secret) and (number_of_tries < max_tries-1):
+    # you could also start number of tries at 0
+    print ("Wrong!")
+    # if guess is out of bounds, tell them
+    if (guess < 1) or (guess > 10):
+        print("That's not between 1 and 10! ")
+    # If guess it too low, tell them
+    if guess < secret: 
+        print("Too low! ")
+    # If guess is too high, tell them
+    else:
+        print("Too high! ")
+    # Ask for a different guess
+    guess = int(input("Guess again! "))
+    number_of_tries += 1
+# If I've reached this point, guess == secret
+if guess == secret:
+    print("You guessed correctly! ")
+else:
+    print("You lose, :(" )
