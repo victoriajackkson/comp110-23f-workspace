48a3e709bd6077bf26376fd4412f687a3852ee23
Victoria Jackson <vjack2@email.unc.edu>
2023-10-09 13:47:03 -0400

    Progress on EX04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,21 +1,22 @@
-"""EX04- Work Utils"""
+"""EX04- Work Utils."""
 __author__ = "730400711"
 
-def all(list: list[int], number:int) -> bool:
-    """Returns T/F if integer matches any in list."""
-    index: int = 0
 
+def all(list: list[int], number: int) -> bool:
+    """Returns T/F if integer inputted matches any in list given."""
+    index: int = 0
     if list == []:
         return False
     while index < len(list):
-            if number != list[index]:
-                return False
-            index += 1
+        if number != list[index]:
+            return False
+        index += 1
     else:
         return True
 
+
 def max(input: list[int]) -> int:
-    """Given a list, the largest number is returned"""
+    """Given a list, the largest number is returned."""
     index: int = 0
     index2: int = 1
     if len(input) == 0:
@@ -28,9 +29,12 @@ def max(input: list[int]) -> int:
             index2 += 1
         return value
 
+
 def is_equal(list1: list[int], list2: list[int]) -> bool:
-    """Returns T/F if elements are equal to one another in lists"""
+    """Returns T/F if elements are true to one another at each index in lists."""
     index: int = 0
+    if len(list1) != len(list2):
+        return False
     while index < len(list1) and len(list2):
         if list1[index] != list2[index]:
             return False
