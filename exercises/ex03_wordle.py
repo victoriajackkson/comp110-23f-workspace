6b8baa86fd86625d1a33b335a5bb188ae515a0c5
Victoria Jackson <vjack2@email.unc.edu>
2023-10-08 20:47:42 -0400

    Progress on EX04

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,10 +1,10 @@
-"""EX03- Structured Wordle"""
+"""EX03- Structured Wordle."""
 __author__ = "730400711"
 
 
 # Creating function that will see if character from guess is found in secret word
 def contains_char(guess: str, char: str) -> bool:
-    """ Returns T/F character found in string"""
+    """Returns T/F character found in string."""
     assert len(char) == 1
     index: int = 0
     while index < len(guess):
@@ -16,8 +16,8 @@ def contains_char(guess: str, char: str) -> bool:
 
 
 # Creating function for emojifying guessed characters found (or not) in secret word
-def emojified(guess:str, secret: str) -> str:
-    """Returns string of emojis whose color codifies correct/incorrect letters found in secret word"""
+def emojified(guess: str, secret: str) -> str:
+    """Returns string of emojis whose color codifies correct/incorrect letters found in secret word."""
     assert len(guess) == len(secret)
     index: int = 0
     WHITE_BOX: str = "\U00002B1C"
@@ -30,39 +30,41 @@ def emojified(guess:str, secret: str) -> str:
         if guess[index] == secret[index]:
             box = box + GREEN_BOX
         else: 
-            if contains_char(secret,guess[index]):
+            if contains_char(secret, guess[index]):
                 box = box + YELLOW_BOX
             else:
                 box = box + WHITE_BOX
         index = index + 1
     return box
 
+
 # Creating function for user input for guess word. Checking if guess is correct length.
-def input_guess(expected:int) -> str:
+def input_guess(expected: int) -> str:
     """Returns prompt for user to input guess of expected length, correct length isn't given, prompt again."""
-    guess: str = input(f"Enter a {expected} character word: ")
-    while len(guess) != expected:
-        guess: str = input(f"That wasn't {expected} chars! Try again: ")
-    return guess
+    player_guess = input(f"Enter a {expected} character word: ")
+    while len(player_guess) != expected:
+        player_guess = input(f"That wasn't {expected} chars! Try again: ")
+    return player_guess
+
 
 # Declaring main function
 def main() -> None:
     """The entrypoint of the program and main game loop."""
-turns: int = 1
-win: str = "codes"
-result: str = ""
+    turns: int = 1
+    win: str = "codes"
+    result: str = ""
+    while turns <= 6 and result != win:
+        print(f"=== Turn {turns}/6 ===")
+        result = input_guess(len(win))
+        print(emojified(result, win))
+        turns = turns + 1
 
-while turns <= 6 and result != win:
-    print(f"=== Turn {turns}/6 ===")
-    result = input_guess(len(win))
-    print(emojified(result, win))
-    turns = turns +1
+    # Printing result if user is out of turns and incorrect, or if they've guessed correctly at or under 6 turns.
+    if turns >= 6 and result != win:
+        print("X/6 - Sorry, try again tomorrow!")
+    if result == win:
+        print(f"You won in {turns-1}/6 turns!")
 
-# Printing result if user is out of turns and incorrect, or if they've guessed correctly at or under 6 turns.
-if turns >= 6 and result != win:
-    print("X/6 - Sorry, try again tomorrow!")
-if result == win:
-    print(f"You won in {turns-1}/6 turns!")
 
 if __name__ == "__main__":
     main()
---
+++ exercises/ex04_utils.py
@@ -0,0 +1,39 @@
+"""EX04- Work Utils"""
+__author__ = "730400711"
+
+def all(list: list[int], number:int) -> bool:
+    """Returns T/F if integer matches any in list."""
+    index: int = 0
+
+    if list == []:
+        return False
+    while index < len(list):
+            if number != list[index]:
+                return False
+            index += 1
+    else:
+        return True
+
+def max(input: list[int]) -> int:
+    """Given a list, the largest number is returned"""
+    index: int = 0
+    index2: int = 1
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty List")
+    else:
+        value: int = input[index]
+        while index2 < len(input):
+            if value < input[index2]:
+                value = input[index2]
+            index2 += 1
+        return value
+
+def is_equal(list1: list[int], list2: list[int]) -> bool:
+    """Returns T/F if elements are equal to one another in lists"""
+    index: int = 0
+    while index < len(list1) and len(list2):
+        if list1[index] != list2[index]:
+            return False
+        index += 1
+    else:
+        return True
