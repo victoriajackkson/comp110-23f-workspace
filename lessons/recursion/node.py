4008dfea5a1ccb9f97bba9790e713c1563fbef93
Victoria Jackson <vjack2@email.unc.edu>
2023-11-20 11:39:44 -0500

    Progress on CQ08

--- lessons/recursion/node.py
+++ lessons/recursion/node.py
@@ -1,6 +1,7 @@
 """Node Class."""
 
 from __future__ import annotations
+__author__ = "730400711"
 
 
 class Node:
@@ -8,11 +9,11 @@ class Node:
     
     data: int
     next: Node | None
-    
+
     def __init__(self, data: int, next: Node | None):
         """Construct Node."""
         self.data = data
-        self. next = next
+        self.next = next
         
     def __str__(self) -> str:
         """Produce a string visualization of the linked list."""
@@ -22,11 +23,27 @@ class Node:
         else:
             return f"{self.data} -> {self.next}"
         
-    def head(self):
-        return None
-    
-    def tail(self):
-        return None
-    
-    def last(self):
-        return None
+    def head(self) -> int:
+        """Return data attribute for first element in linked list."""
+        return self.data
+
+    def tail(self) -> Node| None: 
+        """Returns linked list of every element minus the head."""
+        if self.next is None:
+            return None
+        else:
+            return self.next
+
+    def last(self) -> int:
+        """Return the data of the last element in the linked list."""
+        current = self
+        while current.next is not None:
+            current = current.next
+        return current.data
+
+# node_c: Node = Node(2, None) #base case
+# node_b: Node = Node(1, node_c)
+# node_a: Node = Node(0, node_b) #head of list
+
+# print(node_a.next.next.data)
+# print(node_a.next.next.next)
