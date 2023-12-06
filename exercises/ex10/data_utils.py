4d67799c1427ccd2c02018b01bea54b06339c2a1
Victoria Jackson <vjack2@email.unc.edu>
2023-12-06 15:12:43 -0500

    Progress on EX10

--- exercises/ex10/data_utils.py
+++ exercises/ex10/data_utils.py
@@ -9,12 +9,13 @@ from csv import DictReader
 def read_csv_rows(filename: str) -> list[dict[str, str]]:
     """Read csv file and return as a list of dicts with the headers as the keys."""
     data_rows: list[dict[str, str]] = []
-    file_handle = open(filename, "r", encoding = "utf8")
+    file_handle = open(filename, "r", encoding="utf8")
     csv_reader = DictReader(file_handle)
     for row in csv_reader:
         data_rows.append(row)
     file_handle.close()
     return data_rows
+
 
 def column_values(table: list[dict[str, str]], header: str) -> list[str]:
     """Returns list of all values under a specific header."""
@@ -25,6 +26,7 @@ def column_values(table: list[dict[str, str]], header: str) -> list[str]:
         result.append(elem[header])
     return result
 
+
 def columnar(table: list[dict[str, str]]) -> dict[str, list[str]]:
     """Reformat data so it is a dictionary with column headers as keys."""
     result: dict[str, list[str]] = {}
@@ -34,6 +36,7 @@ def columnar(table: list[dict[str, str]]) -> dict[str, list[str]]:
         # for each key(header), make a dictionary entry with all the columns
         result[key] = column_values(table, key)
     return result
+
 
 def head(table: dict[str, list[str]], N: int) -> dict[str, list[str]]:
     """Produces new column with only certain amount of rows of data for each column."""
@@ -50,6 +53,7 @@ def head(table: dict[str, list[str]], N: int) -> dict[str, list[str]]:
         result[column_name] = N_values
     return result
 
+
 def select(table: dict[str, list[str]], column_names: list[str]) -> dict[str, list[str]]:
     """Produces new table with specific subset of original columns."""
     result: dict[str, list[str]] = {}
@@ -57,25 +61,31 @@ def select(table: dict[str, list[str]], column_names: list[str]) -> dict[str, li
         result[name] = table[name]
     return result
 
+
 def concat(table_1: dict[str, list[str]], table_2: dict[str, list[str]]) -> dict[str, list[str]]:
     """Produces new table with two column-based tables combined."""
     result: dict[str, list[str]] = {}
     # loops through each column in first parameter
     for column in table_1:
         result[column] = table_1[column]
+    # loops through each column in second parameter
     for column in table_2:
         if column in result:
+            # merges lists together if same keys
             result[column] += table_2[column]
         else:
             result[column] = table_2[column]
     return result
 
+
 def count(input: list[str]) -> dict[str, int]:
     """Produces list where each key is unique value in given list and is associated with the count of times it appeared in input."""
     result: dict[str, int] = {}
     for item in input:
+        # adding to count if key already in the result
         if item in result:
             result[item] += 1
         else:
+            # starting count if not
             result[item] = 1
     return result
